f2eb78c12f77aafa13073c6b3b77c946f2ee3593
Joshua <joshdante@hotmail.com>
2023-03-16 17:56:58 -0400

    phase 3 complete

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -2,7 +2,7 @@
 
 const { Model, Validator } = require("sequelize");
 
-const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,15 +1,35 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { restoreUser } = require("../../utils.js/auth");
+
+router.use(restoreUser);
 
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
+//  router.get("/restore-user", (req, res) => {
+//   return res.json(req.user);
+// });
 
+// const { setTokenCookie } = require("../../utils.js/auth");
+// const { User } = require("../../db/models");
 
-
-
-
-
-
+// router.get("/set-token-cookie", async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: "Demo-lition",
+//     },
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+// const { requireAuth } = require('../../utils.js/auth');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
 
 module.exports = router;
