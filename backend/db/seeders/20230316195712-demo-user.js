ba3fd9be39d7341f37bc083f8211d8289a14d367
Joshua <joshdante@hotmail.com>
2023-03-16 16:08:00 -0400

    Phase 3 table and seeds done

---
+++ backend/db/seeders/20230316195712-demo-user.js
@@ -0,0 +1,31 @@
+"use strict";
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.bulkInsert(options, [
+      { email: "demo@user.io", username: "Demo-lition", hashedPassword: bcrypt.hashSync("password") },
+    ], {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: {[Op.in]: ['Demo-lition']}
+    })
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
