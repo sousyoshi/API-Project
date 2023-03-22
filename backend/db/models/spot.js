ab6d542bf88de4b35b30ab4beab2261d4c1642c9
Joshua <joshdante@hotmail.com>
2023-03-22 14:46:15 -0400

    Foreign key rework

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
       primaryKey: true,
       autoIncrement: true
     },
-      ownerId: {type: DataTypes.INTEGER, references: {model: User, key: 'id'}},
+      ownerId: {type: DataTypes.INTEGER, references: {model: "User", key: 'id'}},
       address: DataTypes.STRING,
       city: DataTypes.STRING,
       state: DataTypes.STRING,
