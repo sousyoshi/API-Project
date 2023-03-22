c8289ff8a6728c75db8a040de67b1e90faf0cd1f
Joshua <joshdante@hotmail.com>
2023-03-22 19:35:40 -0400

    Spots post route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -16,10 +16,10 @@ router.get("/", async (req, res) => {
     let spot = spotList[i];
     for (let j = 0; j < spot.SpotImages.length; j++) {
       let image = spot.SpotImages[j];
-        if (image.preview === true) {
+      if (image.preview === true) {
         spot.previewImage = image.url;
       }
-        if (!spot.previewImage) {
+      if (!spot.previewImage) {
         spot.previewImage = "no image found";
       }
     }
@@ -30,9 +30,7 @@ router.get("/", async (req, res) => {
         spotId: spot.id,
       },
       attributes: {
-        include: [
-          [sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]
-        ],
+        include: [[sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]],
       },
     });
 
@@ -44,13 +42,33 @@ router.get("/", async (req, res) => {
   res.json({ spotList });
 });
 
+router.post("/", requireAuth, async (req, res) => {
+  const { user } = req;
+  const { address, city, state, country, lat, lng, name, description, price } = req.body;
+  const newSpot = await Spot.create({
+    ownerId: user.id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
+
+  res.status(201).json(newSpot);
+});
+
 router.get("/current", requireAuth, async (req, res) => {
-  
+  let user = req.user;
 
   const Spots = await Spot.findAll({
     include: {
-      model: SpotImage}
-    });
+      model: SpotImage,
+    },
+  });
 
   return res.json({});
 });
