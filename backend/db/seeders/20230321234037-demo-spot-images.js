788531f7d2e951570fd717d598cd2af8fdd50c37
Joshua <joshdante@hotmail.com>
2023-03-22 12:38:48 -0400

    Finished get all spots

--- backend/db/seeders/20230321234037-demo-spot-images.js
+++ backend/db/seeders/20230321234037-demo-spot-images.js
@@ -1,8 +1,38 @@
-'use strict';
-
+"use strict";
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
+    options.tableName = "SpotImages";
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+          spotId: 1,
+          url: "demo.png",
+          preview: false,
+        },
+        {
+          spotId: 2,
+          url: "Bat-Cave.png",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "Daily-Planet.png",
+          preview: true,
+        },
+        {
+          spotId: 4,
+          url: "Beach-house.png",
+          preview: true
+        },
+      ],
+      {}
+    );
     /**
      * Add seed commands here.
      *
@@ -11,15 +41,17 @@ module.exports = {
      *   name: 'John Doe',
      *   isBetaMember: false
      * }], {});
-    */
+     */
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
+    options.tableName = "ReviewImages"
+    return queryInterface.bulkDelete(options)
     /**
      * Add commands to revert seed here.
      *
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
-  }
+  },
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,23 +1,52 @@
 const express = require("express");
 
-const { Spot, Review, Booking, sequelize } = require("../../db/models");
+const { Spot, Review, Booking, SpotImage, sequelize } = require("../../db/models");
 const { requireAuth } = require("../../utils.js/auth");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const Spots = await Spot.findAll({});
+  const Spots = await Spot.findAll({ include: { model: SpotImage } });
 
+  let spotList = [];
+  Spots.forEach((spot) => {
+    spotList.push(spot.toJSON());
+  });
 
+  for (let i = 0; i < spotList.length; i++) {
+    let spot = spotList[i];
+    for (let j = 0; j < spot.SpotImages.length; j++) {
+      let image = spot.SpotImages[j];
+      if (image.preview === true) {
+        spot.previewImage = image.url;
+      }
+      if (!spot.previewImage) {
+        spot.previewImage = "no image found";
+      }
+    }
+    delete spot.SpotImages;
+    const reviewData = await Review.findOne({
+      where: {
+        spotId: spot.id,
+      },
+      attributes: {
+        include: [[sequelize.fn("AVG", sequelize.col("stars")), "avgRating"]],
+      },
+    });
 
+    let data = parseFloat(reviewData.toJSON().avgRating).toFixed(1);
+    
+    spot.avgRating = +data;
+  }
 
-  return res.json({Spots})
-
+  res.json({ spotList });
 });
 
 router.get("/current", requireAuth, async (req, res) => {
+  let user = req.user
+  console.log(user)
   const Spots = await Spot.findAll();
 
-  return res.json({ });
+  return res.json({});
 });
 
 module.exports = router;
