67ec4a541f0511ce363f4397be8d465dc9059a24
Joshua <joshdante@hotmail.com>
2023-03-22 16:42:50 -0400

    cmon please work

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       User.hasMany(models.Booking, { foreignKey: "userId" });
-      User.hasMany(models.Spot, { as: "ownerId" });
+      User.hasMany(models.Spot, {foreignKey: "userId",  as: "ownerId" });
       User.hasMany(models.Review, { foreignKey: "userId" });
     }
   }
