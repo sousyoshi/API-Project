d81715f10f5b09cbe8ac85e077dd01d1b25e5da9
Joshua <joshdante@hotmail.com>
2023-04-08 13:16:23 -0400

    auth me done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
-// frontend/src/App.js
 import React, { useState, useEffect } from "react";
-import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
 import { useDispatch } from "react-redux";
+import { Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
-import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
@@ -18,19 +14,9 @@ function App() {
   return (
     <>
       <Navigation isLoaded={isLoaded} />
-      {isLoaded && (
-        <Switch>
-          <Route path="/login">
-            <LoginFormPage />
-          </Route>
-          <Route path="/signup">
-            <SignupFormPage />
-          </Route>
-        </Switch>
-      )}
+      {isLoaded && <Switch></Switch>}
     </>
   );
 }
-
 
 export default App;
