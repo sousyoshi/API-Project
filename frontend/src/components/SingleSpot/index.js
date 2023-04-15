fe1d82e7545a42cb80eeb4a9c2d70a6a686c1765
Joshua <joshdante@hotmail.com>
2023-04-15 18:08:21 -0400

    fixed up more spot details

--- frontend/src/components/SingleSpot/index.js
+++ frontend/src/components/SingleSpot/index.js
@@ -15,10 +15,14 @@ const SingleSpot = () => {
   const reviews = useSelector((state) => Object.values(state.reviews)[0]);
   const reviewVal = Object.values(reviews);
   const sessionUser = useSelector((state) => state.session.user);
-  console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS", spot);
+  console.log("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS", reviewVal);
 
   const reviewArr = reviewVal.filter((review) => {
     return review?.userId === sessionUser?.id;
+  });
+
+  const sortedReviews = reviewVal.sort((a, b) => {
+    return new Date(b.createdAt) - new Date(a.createdAt);
   });
 
   const userOwnedSpot = sessionUser?.id === spot.Owner?.id;
@@ -27,7 +31,7 @@ const SingleSpot = () => {
   useEffect(() => {
     dispatch(getSingleSpotThunk(spotId));
     dispatch(getReviewsThunk(spotId));
-  }, [dispatch, spotId]);
+  }, [dispatch]);
 
   const comingSoon = () => {
     alert("Feature coming soon.");
@@ -55,21 +59,42 @@ const SingleSpot = () => {
       <p>{spot.description}</p>
       <section className="calloutBox">
         <h3>${spot.price?.toLocaleString("en-US")} night </h3>
-        <p>{spot.avgStarRating?.toFixed(2)} </p>
-        <p> &#x2022;</p>
-        <p>{spot.numReviews > 1 ? `${spot.numReviews} reviews` : `${spot.numReviews} review`}</p>
+        <i class="fa-solid fa-star"></i>
+        <p>{spot.avgStarRating?.toFixed(2) || "New"} </p>
+        {spot.numReviews === 0 ? null : (
+          <div>
+            {" "}
+            <p> &#x2022;</p>
+            <p>{spot.numReviews > 1 ? `${spot.numReviews} reviews` : `${spot.numReviews} review`}</p>
+          </div>
+        )}
+
         <button onClick={comingSoon} className="reserve">
           Reserve
         </button>
       </section>
       <div>
-        <h2>{spot.avgStarRating?.toFixed(2)} </h2> <h2>&#x2022;</h2><h2>{spot.numReviews > 1 ? `${spot.numReviews} reviews` : `${spot.numReviews} review`}</h2>
+        <i class="fa-solid fa-star"></i> <h2>{!!reviewVal.length ? spot.avgStarRating?.toFixed(2) : "New"}</h2>
+        {spot.numReviews === 0 ? null : (
+          <div>
+            <h2>&#x2022;</h2>
+            <h2>{spot.numReviews >= 1 ? `${spot.numReviews} reviews` : `${spot.numReviews} review` ? spot.numReviews === 0 : null}</h2>
+          </div>
+        )}
       </div>
+      {!!sessionUser && !reviewArr.length && !userOwnedSpot && (
+        <div>
+          {" "}
+          <OpenModalButton buttonText={"Post your Review"} modalComponent={<PostReviewModal spot={spot} />} />
+        </div>
+      )}
       <ul>
-        {reviewVal.map((review) => {
+        {!reviewVal.length && !userOwnedSpot ? "Be the first to post a review!": sortedReviews.map((review) => {
           return (
             <li key={review?.id}>
-              {review.User.firstName}, {new Date(review.createdAt.slice(0,10)).toLocaleDateString('en-US',{month:'long', year:'numeric'})}, {review.review}{" "}
+              {review.User.firstName},{" "}
+              {new Date(review.createdAt.slice(0, 10)).toLocaleDateString("en-US", { month: "long", year: "numeric" })},{" "}
+              {review.review}{" "}
               {sessionUser && sessionUser?.id === review.User.id && (
                 <OpenModalButton buttonText={"Delete"} modalComponent={<DeleteReviewModal reviewVal={reviewVal} spotId={spotId} />} />
               )}
@@ -77,12 +102,6 @@ const SingleSpot = () => {
           );
         })}
       </ul>
-      {!!sessionUser && !reviewArr.length && !userOwnedSpot && (
-        <div>
-          {" "}
-          <OpenModalButton buttonText={"Post your Review"} modalComponent={<PostReviewModal spot={spot} />} />
-        </div>
-      )}
     </main>
   );
 };
