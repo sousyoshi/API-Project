41b0d37d2c1af27aebf7fdab08d45e722201c504
Joshua <joshdante@hotmail.com>
2023-04-16 18:52:48 -0400

    create spot styled!

--- frontend/src/components/SpotForm/index.js
+++ frontend/src/components/SpotForm/index.js
@@ -22,6 +22,7 @@ const SpotForm = ({ spot, formType }) => {
   const [newUrl3, setUrl3] = useState({ url: "", preview: true });
   const [newUrl4, setUrl4] = useState({ url: "", preview: true });
   const [newUrl5, setUrl5] = useState({ url: "", preview: true });
+  const [hasSubmitted, setHasSubmitted] = useState(false);
   const [valErrors, setValErrors] = useState({});
 
   useEffect(() => {
@@ -37,26 +38,26 @@ const SpotForm = ({ spot, formType }) => {
   }, [spot]);
 
   useEffect(() => {
-    const errors = {};
-    if (!country?.length) errors.country = "Country is required";
-    if (!city?.length) errors.city = "City is required";
-    if (!address?.length) errors.address = "Address is required";
-    if (!state?.length) errors.state = "State is required";
-    if (description?.length < 30) errors.description = "Description needs a minimum of 30 characters";
-    if (!name?.length) errors.name = "Name is required";
-    if (!price?.length) errors.price = "Price is required";
-    if (!newUrl?.url.length) errors.newUrl = "Preview image is required";
-    if (!newUrl?.url.endsWith(".png") && !newUrl?.url.endsWith(".jpeg") && !newUrl?.url.endsWith(".jpg"))
-      errors.newUrlImages = "Image URL must end in .png, .jpg, or .jpeg";
+    if (hasSubmitted) {
+      const errors = {};
+      if (!country?.length) errors.country = "Country is required";
+      if (!city?.length) errors.city = "City is required";
+      if (!address?.length) errors.address = "Address is required";
+      if (!state?.length) errors.state = "State is required";
+      if (description?.length < 30) errors.description = "Description needs a minimum of 30 characters";
+      if (!name?.length) errors.name = "Name is required";
+      if (!price?.length) errors.price = "Price is required";
+      if (!newUrl?.url.length) errors.newUrl = "Preview image is required";
+      if (!newUrl?.url.endsWith(".png") && !newUrl?.url.endsWith(".jpeg") && !newUrl?.url.endsWith(".jpg"))
+        errors.newUrlImages = "Image URL must end in .png, .jpg, or .jpeg";
 
-    setValErrors(errors);
-  }, [country, city, address, description, name, price, state, newUrl]);
-
-
-
+      setValErrors(errors);
+    }
+  }, [country, city, address, description, name, price, state, newUrl, hasSubmitted]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setHasSubmitted(true);
     const newSpot = { ...spot, ownerId: user.id, country, lat: 88, lng: 150.595959, city, address, description, name, price, state };
     console.log(newSpot);
     if (formType === "Update your spot") {
@@ -66,41 +67,61 @@ const SpotForm = ({ spot, formType }) => {
     } else {
       const newerSpot = await dispatch(createSpotThunk(newSpot, [newUrl, newUrl2, newUrl3]));
       if (newerSpot) {
-        const newNew = newerSpot
+        const newNew = newerSpot;
         history.push(`/spots/${newNew.id}`);
       }
     }
+
+    setAddress("");
+    setCountry("");
+    setAddress("");
+    setCity("");
+    setState("");
+    setDescription("");
+    setPrice("");
+    setName("");
+    setHasSubmitted(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="spotForm" onSubmit={handleSubmit}>
       <h2>{formType}</h2>
-      <h3>Where's your place located?</h3>
-      <p>Guests will only see your exact address once they booked a reservation.</p>
-      <label className="countryLabel">
-        Country: {valErrors.country && <p className="errors">{valErrors.country}</p>}
-        <input placeholder="Country" type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
-      </label>
-      <label>
-        Street Address: {valErrors.address && <p className="errors">{valErrors.address}</p>}
-        <input placeholder="Address" type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
-      </label>
-      <label>
-        City: {valErrors.city && <p className="errors">{valErrors.city}</p>}
-        <input placeholder="City" type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-      </label>
-      <label>
-        State: {valErrors.state && <p className="errors">{valErrors.state}</p>}
-        <input placeholder="STATE" type="text" value={state} onChange={(e) => setState(e.target.value)} />
-      </label>
+      <fieldset className="firstField">
+        <h3>Where's your place located?</h3>
+        <p>Guests will only see your exact address once they booked a reservation.</p>
+        <div className="countryStateDiv">
+           <label className="countryLabel">
+          Country: {valErrors.country && <div className="errors">{valErrors.country}</div>}
+          <input placeholder="Country" type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
+        </label>
+        <label>
+          Street Address: {valErrors.address && <div className="errors">{valErrors.address}</div>}
+          <input placeholder="Address" type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
+        </label>{" "}
+        </div>
+
+        <div className="stateDiv">
+          <label>
+            City: {valErrors.city && <div className="errors">{valErrors.city}</div>}
+            <input placeholder="City" type="text" value={city} onChange={(e) => setCity(e.target.value)} />
+          </label>
+          <label>
+            State: {valErrors.state && <div className="errors">{valErrors.state}</div>}
+            <input placeholder="STATE" type="text" value={state} onChange={(e) => setState(e.target.value)} />
+          </label>
+        </div>
+      </fieldset>
+
       <h3>Describe your place to guests</h3>
       <p>
         Mention the best features of your space, any special amentities like fast wifi or parking, and what you love about the
         neighborhood.
       </p>
-      <label>
-        {valErrors.description && <p className="errors">{valErrors.description}</p>}
+      <label className="textarea">
+        {valErrors.description && <div className="errors">{valErrors.description}</div>}
         <textarea
+          rows={6}
+          cols={80}
           placeholder="Please write at least 30 characters"
           type="text"
           value={description}
@@ -111,21 +132,23 @@ const SpotForm = ({ spot, formType }) => {
       <h3>Create a title for your spot</h3>
       <p>Catch guests' attention with a spot title that highlights what makes your place special.</p>
       <label>
-        {valErrors.name && <p className="errors">{valErrors.name}</p>}
+        {valErrors.name && <div className="errors">{valErrors.name}</div>}
         <input placeholder="Name of your spot" type="text" value={name} onChange={(e) => setName(e.target.value)} />
       </label>
       <h3>Set a base price for your spot</h3>
       <p>Competitve pricing can help your listing stand out and rank higher in search results</p>
       <label>
-        {valErrors.price && <p className="errors">{valErrors.price}</p>}
-        $ <input placeholder="Price per night (USD)" type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+        {valErrors.price && <div className="errors">{valErrors.price}</div>}
+        <span className="priceInput">
+          <input placeholder="Price per night (USD)" type="text" value={price} onChange={(e) => setPrice(e.target.value)} />
+        </span>
       </label>
       {formType === "Create a new Spot" ? (
         <div>
           <h3>Liven up your spot with photos</h3>
           <p>Submit a link to at least one photo to publish your spot.</p>
-          {valErrors.newUrl && <p className="errors">{valErrors.newUrl}</p>}
-          {valErrors.newUrlImages && <p className="errors">{valErrors.newUrlImages}</p>}
+          {valErrors.newUrl && <div className="errors">{valErrors.newUrl}</div>}
+          {valErrors.newUrlImages && <div className="errors">{valErrors.newUrlImages}</div>}
           <input
             placeholder="Preview Image URL"
             type="text"
@@ -159,7 +182,9 @@ const SpotForm = ({ spot, formType }) => {
         </div>
       ) : null}
 
-      <button type="submit">{formType}</button>
+      <button className="submitForm" type="submit">
+        {formType}
+      </button>
     </form>
   );
 };
